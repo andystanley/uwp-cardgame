38debf6d6f3ac778780f639830f19c34688b0c98
Andrew Stanley <andystanley@hotmail.ca>
2017-04-08 22:27:00 -0400

    Added New Game and View Rules Buttons to the GamePage.  Also added full functionality

--- 21CardGame/Presentation Logic/GamePage.xaml.cs
+++ 21CardGame/Presentation Logic/GamePage.xaml.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using UniversalCardGame;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -217,6 +218,32 @@ namespace _21CardGame
             _player2Point.Text = "0";
             _player3Point.Text = "0";
             _player4Point.Text = "0";
+        }
+
+        private async void OnViewRules(object sender, RoutedEventArgs e)
+        {
+            // Store the text from instructions.txt in a variable
+            string rules = File.ReadAllText("Assets/rules.txt");
+
+            // Display the instructions in a MessageDialog
+            var dialog = new MessageDialog(rules, "Game Rules");
+            await dialog.ShowAsync();
+        }
+
+        private async void OnNewGame(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MessageDialog("Are you sure wish to start a new game?  This will reset the current game!");
+
+            dialog.Commands.Add(new UICommand("Yes"));
+            dialog.Commands.Add(new UICommand("Cancel"));
+
+            var result = await dialog.ShowAsync();
+
+            if (result.Label == "Yes")
+            {
+                // Navigates to the Game Page
+                this.Frame.Navigate(typeof(MainPage));
+            }
         }
     }
 }
